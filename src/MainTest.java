6e2b43081a58bc3d6c8da86911ad45f2e069a19e
Francesco <Francesco@192.168.1.2>
2017-10-18 10:02:33 +0200

    Migliorata interfaccia aggiunti collegamenti a scelta 1 e 2

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,7 @@
 /* 
 	@copy Coded by Francesco Garofalo 2017
  */
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
@@ -12,6 +13,7 @@ public class MainTest {
 	public static void main (String args []) {	
 
 		logger = Logger.getLogger("com.nomeProgetto"); //Serve per stampare messaggi a sistema
+		Gestore gestore = new Gestore();
 		//UserInterface e scelta opzioni
 		/*
 		 * Prima scelta Get istanza
@@ -19,8 +21,29 @@ public class MainTest {
 		String scelta1 = JOptionPane.showInputDialog("Definisci l'operazione che vuoi effettuare! 1.Usa un'istanza esistente| 2.Crea una nuova istanza");
 		int s1 = Integer.parseInt(scelta1);
 		if (s1 == 1) {
-			String scelta2 = JOptionPane.showInputDialog("Definisci il nome dell'istanza:");
-
+			String istanza = JOptionPane.showInputDialog("Definisci il nome dell'istanza:");
+			try {
+				gestore.getLavoratoriDB(istanza);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				gestore.getLavoriDB(istanza);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String criterio1 = JOptionPane.showInputDialog("Definisci il criterio per gli accompagnatori in servizio. 1.Cmin, 2.Cmax, 3.Random");
+			int c1 = Integer.parseInt(criterio1);
+			String criterio2 = JOptionPane.showInputDialog("Definisci il criterio per gli accompagnatori non in servizio. 1.Cmin, 2.Cmax, 3.Random");
+			int c2 = Integer.parseInt(criterio2);
+			gestore.trovaAccompagnatore(c1, c2);
+			System.out.println("Ho ordinato i passeggeri.");
+			JOptionPane.showInputDialog("Stampa");
+			gestore.stampaArrayLavoratori();
+			gestore.stampaArrayLavori();
+			
 		} else if(s1 == 2) {
 			String scelta2 = JOptionPane.showInputDialog("Definisci il numero di passeggeri");
 			int n = Integer.parseInt(scelta2);
@@ -33,17 +56,28 @@ public class MainTest {
 			String criterio2 = JOptionPane.showInputDialog("Definisci il criterio per gli accompagnatori non in servizio. 1.Cmin, 2.Cmax, 3.Random");
 			int c2 = Integer.parseInt(criterio2);
 			String istanza = JOptionPane.showInputDialog("Definisci il nome dell'istanza:");
-			Gestore gestore = new Gestore();
 			gestore.aggiungiAccompagnatori(m, L, istanza);
 			gestore.aggiungiPasseggeri(n, istanza);
 			gestore.ordinaPasseggeriInArray();
 			String perstok = JOptionPane.showInputDialog("Vuoi rendere persistente l'istanza? 1.Si 2.NO");
 			int scelta = Integer.parseInt(perstok);
 			if(scelta == 1) {
-
+				try {
+					gestore.popolaLavoratoriDB();
+				} catch (SQLException e) {
+					logger.severe("PERSISTENZA NON RIUSCITA!");
+					e.printStackTrace();
+				}
+				try {
+					gestore.popolaLavoriDB();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					logger.severe("PERSISTENZA NON RIUSCITA!");
+				}
 			}
 			else {
-
+				
 			}
 			gestore.trovaAccompagnatore(c1, c2);
 			System.out.println("Ho ordinato i passeggeri.");
